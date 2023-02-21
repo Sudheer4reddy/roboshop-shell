6e7be0887dddc6bafc31a7e1dee1d4fb7d24f11a
Sudheer4reddy <reddigarisudheer447@gmail.com>
2023-02-21 15:10:54 +0530

    Roboshops Components

---
+++ mongodb.sh
@@ -0,0 +1,7 @@
+cp Configs/mongodb.repo /etc/yum.repos.d/mongo.repo
+yum install mongodb-org -y
+systemctl enable mongod
+systemctl start mongod
+
+
+#update  /etc/mongod.conf from 127.0.0.1 to 0.0.0.0
