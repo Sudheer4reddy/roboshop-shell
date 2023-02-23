50a33be7a58ccb7dd0122ac90c743d39671b93f4
Sudheer4reddy <reddigarisudheer447@gmail.com>
2023-02-23 13:33:02 +0530

    MongoDB in Roboshop

--- mongodb.sh
+++ mongodb.sh
@@ -7,7 +7,7 @@ print_head "installing MongoDB"
 yum install mongodb-org -y &>>${log_file}
 
 print_head "Update MongoDB Listen address"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongo.d.conf &>>${log_file}
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>${log_file}
 
 print_head "Enable MongoDB"
 systemctl enable mongod &>>${log_file}
