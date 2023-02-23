d70fada016f857cf7c6e65b6e4cb69cdf64f7378
Sudheer4reddy <reddigarisudheer447@gmail.com>
2023-02-23 11:55:08 +0530

    frontend in Roboshop

--- mongodb.sh
+++ mongodb.sh
@@ -1,6 +1,15 @@
+source common.sh
+
+print_head "Setup MongoDB Repository"
 cp Configs/mongodb.repo /etc/yum.repos.d/mongo.repo
+
+print_head "installing MongoDB"
 yum install mongodb-org -y
+
+print_head "Enable MongoDB"
 systemctl enable mongod
+
+print_head "starting MongoDB service"
 systemctl start mongod
 
 
