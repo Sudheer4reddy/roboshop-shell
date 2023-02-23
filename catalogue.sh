692d2ce6e4f9f5196e7b59ef40e6dcb66b19ebed
Sudheer4reddy <reddigarisudheer447@gmail.com>
2023-02-23 12:21:52 +0530

    Catalogue in Roboshop

--- catalogue.sh
+++ catalogue.sh
@@ -1,21 +1,47 @@
 source common.sh
 
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
-useradd roboshop
-mkdir /app
-rm -rf /app/*
+print_head "Configure NodeJS Repo"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
+
+print_head "Install NodeJS"
+yum install nodejs -y &>>${log_file}
+
+print_head "Create Roboshop User"
+useradd roboshop &>>${log_file}
+
+print_head "Create Application Directory"
+mkdir /app &>>${log_file}
+
+print_head "Delete Old Content"
+rm -rf /app/* &>>${log_file}
+
+print_head "Downloading Application Content"
 curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
-cd /app
-unzip /tmp/catalogue.zip
-cd /app
-npm install
-cp Configs/catalogue.service /etc/systemd/system/catalogue.service
-systemctl daemon-reload
-systemctl enable catalogue
-systemctl start catalogue
+cd /app &>>${log_file}
 
-cp Configs/mongodb.repo /etc/yum.repos.d/mongodb.repo
-yum install mongodb-org-shell -y
+print_head "Extracting Application Content"
+unzip /tmp/catalogue.zip &>>${log_file}
 
-mongo --host mongodb.roboshop.internal </app/schema/catalogue.js
+print_head "Installing NodeJS Dependencies "
+npm install &>>${log_file}
+
+print_head "Copy SystemD Service File"
+cp Configs/catalogue.service /etc/systemd/system/catalogue.service &>>${log_file}
+
+print_head "Reload SystemD"
+systemctl daemon-reload &>>${log_file}
+
+print_head "Enable Catalogue Service"
+systemctl enable catalogue &>>${log_file}
+
+print_head "Start Catalogue Service"
+systemctl start catalogue &>>${log_file}
+
+print_head "Copy MongoDB Repo File"
+cp Configs/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${log_file}
+
+print_head "Install MongoDB Client"
+yum install mongodb-org-shell -y &>>${log_file}
+
+print_head "Load Schema"
+mongo --host mongodb.roboshop.internal </app/schema/catalogue.js &>>${log_file}
--- mongodb.sh
+++ mongodb.sh
@@ -1,16 +1,16 @@
 source common.sh
 
 print_head "Setup MongoDB Repository"
-cp Configs/mongodb.repo /etc/yum.repos.d/mongo.repo
+cp Configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
 
 print_head "installing MongoDB"
-yum install mongodb-org -y
+yum install mongodb-org -y &>>${log_file}
 
 print_head "Enable MongoDB"
-systemctl enable mongod
+systemctl enable mongod &>>${log_file}
 
 print_head "starting MongoDB service"
-systemctl start mongod
+systemctl start mongod &>>${log_file}
 
 
 #update  /etc/mongod.conf from 127.0.0.1 to 0.0.0.0
