1ece647e70a169f64b8c0fcfc3a02d119e3589db
Sudheer4reddy <reddigarisudheer447@gmail.com>
2023-02-24 13:35:27 +0530

    MySQL in Roboshop

--- mysql.sh
+++ mysql.sh
@@ -25,5 +25,8 @@ systemctl start mysqld &>>${log_file}
 status_check $?
 
 print_head "Set Root Password"
-mysql_secure_installation --set-root-pass RoboShop@1 &>>${log_file}
+echo show databases | mysql -uroot -p${mysql_root_password} &>>${log_file}
+if [ $? -ne 0 ]; then
+  mysql_secure_installation --set-root-pass ${mysql_root_password}  &>>${log_file}
+fi
 status_check $?
